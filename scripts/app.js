f86edcc3b2c47d890ab742b767dfe59b049e56d3
mic7x4 <mic7x4@gmail.com>
2022-04-18 11:24:20 +0200

    finishing with mobile first navbar

---
+++ scripts/app.js
@@ -0,0 +1,17 @@
+//JS
+const menuToggler = document.querySelector('.menu-toggler');
+const navbar = document.querySelector('.navbar');
+const collapsibleMenu = document.querySelector('.collapsible-menu');
+
+menuToggler.addEventListener('click',()=>{
+    navbar.classList.toggle('show-menu');
+    if(navbar.classList.contains('show-menu')){
+        collapsibleMenu.style.maxHeight = collapsibleMenu.scrollHeight + 'px';
+        setTimeout(()=>{
+            collapsibleMenu.style.overflow='visible';
+        })
+    }else{
+        collapsibleMenu.style.maxHeight = '0px';
+        collapsibleMenu.style.overflow = 'hidden';
+    }
+})
